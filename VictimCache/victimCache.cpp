e2dc6ff254e41eff0cc60f3e4a06f410d1ee29c9
Kunal Mohan <kunalmohan99@gmail.com>
2019-10-31 22:41:05 +0530

    victim cache code improvement

--- VictimCache/victimCache.cpp
+++ VictimCache/victimCache.cpp
@@ -1,318 +1,156 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
 using namespace std;
 
+#define MM 64
+#define cache 1
+#define victim 512
+#define blocksize 8
+
+#define read 0
+#define write 1
+#define no_blocks_l1 128
+#define no_blocks_vic 64
+
+long binary_to_decimal(string add){
+	long sum=0;
+	int digit;
+	int i=15;
+	while(i>-1){
+		digit = add[i]-48;
+		sum = sum + (digit*pow(2,i));
+		i--;
+	}
+	return sum;
+}
+
+int min(long arr[], long n){
+	long j, min=10000000000, k;
+	for(j=0;j<n;j++){
+		if(arr[j]<min)
+		k = j;
+	}
+	return k;
+}
+
+
 int main(){
+	long dmcache[no_blocks_l1] = {0};
+	long viccache[no_blocks_vic] = {0};
+	long lruc[no_blocks_vic] = {0};
+	long countr = 0, countw = 0;
+	long op;
+	string add;
+	long read_miss_l1 = 0;
+	long read_miss_vic = 0;
+	long read_hit_l1 = 0;
+	long read_hit_vic = 0;
+	long write_miss_l1 = 0;
+	long write_miss_vic = 0;
+	long write_hit_l1 = 0;
+	long write_hit_vic = 0;
+	long queries = 0;
 
-    freopen("inputfile.txt", "r", stdin); 	
-    freopen("outputfile.txt", "w", stdout);
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 	
+    freopen("output.txt", "w", stdout);
+	#endif
 
-	long long int main_memory_size;
-	long long int l1_cache_size;
-	long long int victim_cache_size;
-	long long int block_size;
-	long long int queries;
-	long long int no_of_hits_l1 = 0;
-	long long int no_of_hits_victim = 0;
-	long long int no_of_misses_l1 = 0;
-	long long int no_of_misses_victim = 0;
-	long long int lines_in_main_memory, lines_in_l1_cache, cache_index, lines_in_victim_cache, block_offset, tag;
+	long no_queries;
+	cin>>no_queries;
 
-	cout<<"-------VICTIM CACHE-------\n"<<endl;
-	cout<<"Memory is byte addressable.\n"<<endl;
+	while(no_queries--){
 
-	//Memory Size
-	cout<<"Size of Main Memory in Bytes : ";
-	cin>>main_memory_size;
-	cout<<main_memory_size<<endl;
+		cin>>op>>add;
+		queries++;
 
-	//Address Size	
-	long long int physical_address=(long long int)log2(main_memory_size); 
-
-	//Size Of L1 Cache
-	cout<<"Size of L1 Cache in Bytes : ";
-	cin>>l1_cache_size;
-	cout<<l1_cache_size<<endl;
-
-	//Size Of Victim Cache
-	cout<<"Size of Victim Cache in Bytes : ";
-	cin>>victim_cache_size;
-	cout<<victim_cache_size<<endl;
-
-	//Block Size
-	cout<<"Block Size in Bytes : ";
-	cin>>block_size;
-	cout<<block_size<<endl;
-
-	lines_in_main_memory=(main_memory_size/block_size);
-	long long int main_memory[lines_in_main_memory][block_size];
-	lines_in_l1_cache=(l1_cache_size/block_size);
-	cache_index=(long long int)log2(lines_in_l1_cache);
-	lines_in_victim_cache=(victim_cache_size/block_size);
-	block_offset=(long long int)log2(block_size);
-	tag=physical_address-cache_index-block_offset;
-
-	//Initializing Main Memory(can be any integer less than 10^18)
-	for(long long int i=0;i<lines_in_main_memory;i++){
-		for(long long int j=0;j<block_size;j++){
-			main_memory[i][j]=i*block_size+j;
-		}
-	} //Sequential Assiging Main Memory
-
-	
-	long long int l1_cache[lines_in_l1_cache][block_size];				// Creating L1 Cache
-	long long int tag_array_of_l1_cache[lines_in_l1_cache];			// Creating Tag array OF L1 Cache
-	long long int valid_array_l1[lines_in_l1_cache];
-	memset(valid_array_l1,0,sizeof(valid_array_l1));		// Creating Valid Array Of L1 Cache
-
-	
-	long long int victim_cache[lines_in_victim_cache][block_size];		//Creating Victim Cache 
-	long long int tag_array_of_victim_cache[lines_in_victim_cache];		// Creating Tag Array OF Victim Cache
-	long long int valid_array_victim[lines_in_victim_cache];
-	memset(valid_array_victim,0,sizeof(valid_array_victim));		// Creating Valid Array Of Victim
-
-	// LRU Counter
-	long long int LRU_in_victim_cache[lines_in_victim_cache];
-	for(long long int i=0;i<lines_in_victim_cache;i++){
-		LRU_in_victim_cache[i]=i;
-	}
-
-	cout<<"Physical address is of "<<physical_address<<" bits."<<endl;
-	cout<<"Tag is of "<<tag<<" bits."<<endl;
-	cout<<"Cache Index is of "<<cache_index<<" bits."<<endl;
-	cout<<"Block Offset is of "<<block_offset<<" bits.\n"<<endl;
-	cout<<"Number of queries entered : ";
-
-	// L1 cache is direct mapped
-	// victim cache is fulong long inty associative
-
-	// addresses to search
-	cin>>queries;
-	cout<<queries<<endl;
-
-	cout<<"Address And Values fetched are folong long intowing : "<<endl;
-	cout<<"Address      Values"<<endl;
-
-	while(queries--){
-		string query;
-		cin>>query;
-
-		long long int query_offset=0;
-		long long int query_index=0;
-		long long int query_tag_l1=0;
-		long long int query_tag_victim=0;
-		long long int counter=1;
-		// calculating offset/word value
-		for(long long int i=physical_address-1;i>=tag+cache_index;i--){
-			query_offset+=((long long int)query[i]-(long long int)'0')*counter;
-			counter*=2;
-		}
-		counter=1;
-
-		// calculating index/line value
-		for(long long int i=tag+cache_index-1;i>=tag;i--){
-			query_index+=((long long int)query[i]-(long long int)'0')*counter;
-			counter*=2;
-		}
-		counter=1;
-		
-		// calculating tag value of L1 cache
-		for(long long int i=tag-1;i>=0;i--){
-			query_tag_l1+=((long long int)query[i]-(long long int)'0')*counter;
-			counter*=2;
-		}
-		counter=1;
-
-		//calcuating tag value for victim cache
-		for(long long int i=tag+cache_index-1;i>=0;i--){
-			query_tag_victim+=((long long int)query[i]-(long long int)'0')*counter;
-			counter*=2;
-		}
-		counter=1;
-
-
-		//Operations
-		if(valid_array_l1[query_index]==1){ // true if  line of l1 cache contains some data 
-
-			if(tag_array_of_l1_cache[query_index]==query_tag_l1){ // true if line of l1 cache contains required address
-
-				no_of_hits_l1++;
-				cout<<query<<" : ";cout<<l1_cache[query_index][query_offset]<<endl;
+		long lineno = binary_to_decimal(add.substr(6,12));
+		long tag = binary_to_decimal(add.substr(0,5));
+		long mod = lineno%no_blocks_l1;
+		if(op == 0){
+			if(dmcache[mod] == tag){
+				read_hit_l1++;
 			}
-			else{  //if line of l1 cache don't contain required data
-			
-				long long int counter2=0;
-				no_of_misses_l1++;
-
-				// going inside victim cache
-				for(long long int i=0;i<lines_in_victim_cache&&counter2==0;i++){ //searching for every line in victim cache
-				
-					if(valid_array_victim[i]==1 && tag_array_of_victim_cache[i]==query_tag_victim){
-						// if line of victim contains some data and that line contains required address
-							counter2++;
-						no_of_hits_victim++;
-						
-						// transferring found block to l1 cache and replacing that with of L1 cache 
-
-							//swap valid bit
-							swap(valid_array_l1[query_index],valid_array_victim[i]);
-							
-							// udating tag array of l1 and victim
-							long long int temp=tag_array_of_l1_cache[query_index]*pow(2,query_index)+query_index;
-							tag_array_of_l1_cache[query_index]=query_tag_l1;
-							tag_array_of_victim_cache[i]=temp;
-							
-							// swap address value
-							for(long long int i1=0;i1<block_size;i1++){
-								swap(l1_cache[query_index][i1],victim_cache[i][i1]);
-							}
-
-							// updating LRU array for furthur action
-							for(long long int i2=0;i2<lines_in_victim_cache;i2++){
-								if(LRU_in_victim_cache[i2]<LRU_in_victim_cache[i])
-									LRU_in_victim_cache[i2]++;
-							}	
-							LRU_in_victim_cache[i]=0;
-								
-							cout<<query<<" : ";cout<<l1_cache[query_index][query_offset]<<endl;
-						}
-				}
-
-				if(counter2==0){		// if required address not found in victim cache
-					no_of_misses_victim++;
-
-					//going inside RAM
-					//finding suitable block in victim cache to hold required value
-					for(long long int i=0;i<lines_in_victim_cache&&counter2==0;i++){
-						if(LRU_in_victim_cache[i]==lines_in_victim_cache-1){	//found suitable line in victim cache to replace
-							
-							counter2++;
-
-							// transfering block form Ram to victim cache
-							tag_array_of_victim_cache[i]=query_tag_victim;
-								for(long long int i1=0;i1<block_size;i1++){
-								victim_cache[i][i1]=main_memory[query_tag_victim][i1];
-							}
-							valid_array_victim[i]=1;
-
-							//updating LRU array for furthur functions
-							for(long long int i2=0;i2<lines_in_victim_cache;i2++){
-								if(LRU_in_victim_cache[i2]<LRU_in_victim_cache[i])
-									LRU_in_victim_cache[i2]++;
-							}	
-							LRU_in_victim_cache[i]=0;
-
+			else{
+				read_miss_l1++;
+				if(dmcache[mod] == 0){
+				dmcache[mod] = tag;
+			}
+				else{
+					int flag = 0;
+					for(long i=0;i<no_blocks_vic;i++){
+						if(viccache[i]==tag){
+							read_hit_vic++;
+							flag = 1;
+							long temp = viccache[i];
+							viccache[i] = dmcache[mod];
+							dmcache[mod] = temp;
+							countr++;
+							lruc[i]=countr;
 						}
 					}
-
-
-					//tranfering block of required data from victim cache to l1 cache
-					for(long long int i=0;i<lines_in_victim_cache;i++){
-
-					if(valid_array_victim[i]==1 && tag_array_of_victim_cache[i]==query_tag_victim){
-							
-							//swaping blocks of l1 and victim cache
-							swap(valid_array_l1[query_index],valid_array_victim[i]);
-							
-							long long int temp=tag_array_of_l1_cache[query_index]*pow(2,query_index)+query_index;
-							tag_array_of_l1_cache[query_index]=query_tag_l1;
-							tag_array_of_victim_cache[i]=temp;
-							
-							for(long long int i1=0;i1<block_size;i1++){
-								swap(l1_cache[query_index][i1],victim_cache[i][i1]);
-							}
-								
-							cout<<query<<" : ";cout<<l1_cache[query_index][query_offset]<<endl;
-						}
+					if(flag == 0){
+						read_miss_vic++;
+						long lru_index = min(lruc, no_blocks_vic);
+						viccache[lru_index] = dmcache[mod];
+						dmcache[mod] = tag;	
 					}
 				}
-
 			}
 		}
-		else{
-			// if there is no data present in line of L1 cache
-			long long int counter2=0;
-				no_of_misses_l1++;
-
-				//going inside victim cache
-				for(long long int i=0;i<lines_in_victim_cache&&counter2==0;i++){
-			
-					if(valid_array_victim[i]==1 && tag_array_of_victim_cache[i]==query_tag_victim){	
-						// if require address is found in victim cache
-						counter2++;
-						no_of_hits_victim++;
-						
-						
-						// swaping block of L1 cache with victim cache
-							swap(valid_array_l1[query_index],valid_array_victim[i]);
-							
-							tag_array_of_l1_cache[query_index]=query_tag_l1;
-							tag_array_of_victim_cache[i]=0;
-							for(long long int i1=0;i1<block_size;i1++){
-								swap(l1_cache[query_index][i1],victim_cache[i][i1]);
-							}
-
-							//updating LRU for victim cache
-							for(long long int i2=0;i2<lines_in_victim_cache;i2++){
-								if(LRU_in_victim_cache[i2]<LRU_in_victim_cache[i])
-									LRU_in_victim_cache[i2]++;
-							}	
-							LRU_in_victim_cache[i]=0;
-								
-							cout<<query<<" : ";cout<<l1_cache[query_index][query_offset]<<endl;
-						}
-				}
-							if(counter2==0){
-								// if required address is not found in victim cache
-					no_of_misses_victim++;
-
-					// finding suitable line in victim cache to fetch block of ram to victim
-					for(long long int i=0;i<lines_in_victim_cache&&counter2==0;i++){
-						if(LRU_in_victim_cache[i]==lines_in_victim_cache-1){	// found suitable line in victim cache
-							counter2++;
-
-							// copying data from RAM to Victim Cache
-							tag_array_of_victim_cache[i]=query_tag_victim;
-								for(long long int i1=0;i1<block_size;i1++){
-								victim_cache[i][i1]=main_memory[query_tag_victim][i1];
-							}
-							valid_array_victim[i]=1;
-
-							// updating LRU array
-							for(long long int i2=0;i2<lines_in_victim_cache;i2++){
-								if(LRU_in_victim_cache[i2]<LRU_in_victim_cache[i])
-									LRU_in_victim_cache[i2]++;
-							}	
-							LRU_in_victim_cache[i]=0;
-
+	
+		if(op == 1){
+			if(dmcache[mod] == tag){
+				write_hit_l1++;
+			}
+			else{
+				write_miss_l1++;
+				if(dmcache[mod] == 0){
+				dmcache[mod] = tag;
+			}
+				else{
+					int flag = 0;
+					for(long i=0;i<no_blocks_vic;i++){
+						if(viccache[i]==tag){
+							write_hit_vic++;
+							flag = 1;
+							long temp = viccache[i];
+							viccache[i] = dmcache[mod];
+							dmcache[mod] = temp;
+							countw++;
+							lruc[i]=countw;
 						}
 					}
-
-					// swaping block of L1 and victim cache
-					for(long long int i=0;i<lines_in_victim_cache;i++){
-					
-					if(valid_array_victim[i]==1 && tag_array_of_victim_cache[i]==query_tag_victim){
-							
-							swap(valid_array_l1[query_index],valid_array_victim[i]);
-							
-							tag_array_of_l1_cache[query_index]=query_tag_l1;
-							tag_array_of_victim_cache[i]=0;
-							for(long long int i1=0;i1<block_size;i1++){
-								swap(l1_cache[query_index][i1],victim_cache[i][i1]);
-							}
-								
-							cout<<query<<" : ";cout<<l1_cache[query_index][query_offset]<<endl;
-						}
+					if(flag == 0){
+						write_miss_vic++;
+						long lru_index = min(lruc, no_blocks_vic);
+						viccache[lru_index] = dmcache[mod];
+						dmcache[mod] = tag;	
 					}
-
-
 				}
-
+			}
 		}
 	}
- 
-	cout<<"\nNumber of hits in l1 cache : "<<no_of_hits_l1<<endl;
-	cout<<"Number of misses in l1 cache : "<<no_of_misses_l1<<endl;
-	cout<<"Number of hits in Victim cache : "<<no_of_hits_victim<<endl;
-	cout<<"Number of misses in Victim cache : "<<no_of_misses_victim<<endl;
+
+	float hit_ratio = float(write_hit_l1+read_hit_l1+read_hit_vic+write_hit_vic)/float(queries);
+	float miss_ratio = 1-hit_ratio;
+
+	cout<<"Main memory size = "<<MM<<" KB"<<endl;
+	cout<<"L1 cache size = "<<cache<<" KB"<<endl;
+	cout<<"Victim cache size = "<<victim<<" Bytes"<<endl;
+	cout<<"Block size = "<<blocksize<<" Bytes"<<endl<<endl;
+	cout<<"Number of write miss in L1 cache = "<<write_miss_l1<<endl;
+	cout<<"Number of write miss in Victim cache = "<<write_miss_vic<<endl;
+	cout<<"Number of write hit in L1 cache = "<<write_hit_l1<<endl;
+	cout<<"Number of write hit in Victim cache = "<<write_hit_vic<<endl;
+	cout<<"Number of read miss in L1 cache = "<<read_miss_l1<<endl;
+	cout<<"Number of read miss in Victim cache = "<<read_miss_vic<<endl;
+	cout<<"Number of read hit in L1 cache = "<<read_hit_l1<<endl;
+	cout<<"Number of read hit in Victim cache = "<<read_hit_vic<<endl<<endl;
+	cout<<"Miss Ratio = "<<miss_ratio<<endl;
+	cout<<"Hit Ratio = "<<hit_ratio<<endl;
+
 	return 0;
-} 
+}
